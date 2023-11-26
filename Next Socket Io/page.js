34704a7d5db755d4b3ce19ee4cdf080ef53538a4
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-11-26 11:28:53 +0530

    socket io update

--- Next Socket Io/page.js
+++ Next Socket Io/page.js
@@ -6,11 +6,19 @@ const socket = io('http://localhost:8000');
 
 const Home = () => {
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         socket.on("GetMessage", (data) => {
-            console.log(data)
+            setMessages((prevMessages) => [...prevMessages, data]);
+            console.log(data);
         });
+        // Clean up the socket connection on unmount
+        return () => {
+            console.log('Component is unmounting'); // Log when the component is unmounting
+            socket.off('GetMessage'); // Unsubscribe from the event
+        };
+
     }, []);
 
     const sendMessage = () => {
@@ -27,6 +35,12 @@ const Home = () => {
                 onChange={(e) => setInput(e.target.value)}
             />
             <button onClick={sendMessage}>Send</button>
+
+            <ul>
+                {messages.map((msg, index) => (
+                    <li key={index}>{msg}</li>
+                ))}
+            </ul>
         </div>
     );
 };
